e4e3d25ff90a7bca2b835f2ddd5f4b6a95fe30d8
kavin-isteer <kavinkr.isteer.com>
2025-07-08 09:53:40 +0530

    Initial commit with project structure

---
+++ iSteer-VMS/src/main/java/com/isteer/vms/ISteerVmsApplication.java
@@ -0,0 +1,13 @@
+package com.isteer.vms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISteerVmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISteerVmsApplication.class, args);
+	}
+
+}
---
+++ iSteer-VMS/src/test/java/com/isteer/vms/ISteerVmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isteer.vms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ISteerVmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
