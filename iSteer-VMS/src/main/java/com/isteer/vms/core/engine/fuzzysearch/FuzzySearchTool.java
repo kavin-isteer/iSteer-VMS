e53bcb40125eb7e7ebacd739148be042863b606f
kavin-isteer <kavinkr.isteer.com>
2025-07-16 11:02:14 +0530

    added .sonarlint in gitignore and modified fuzzysearchtool to use constructor injection and logger using annotation.

--- iSteer-VMS/src/main/java/com/isteer/vms/core/engine/fuzzysearch/FuzzySearchTool.java
+++ iSteer-VMS/src/main/java/com/isteer/vms/core/engine/fuzzysearch/FuzzySearchTool.java
@@ -3,33 +3,33 @@ package com.isteer.vms.core.engine.fuzzysearch;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.isteer.vms.core.engine.lucene.LuceneCpeSearcher;
 import com.isteer.vms.core.engine.model.CpeEntry;
 import com.isteer.vms.core.engine.model.CpeName;
 
+import lombok.extern.log4j.Log4j2;
+@Log4j2
 @Service
 public class FuzzySearchTool {
-	@Autowired
 	LuceneCpeSearcher luceneSearcher;
-	
-	private static final Logger logger = LogManager.getLogger(FuzzySearchTool.class);
+
+	public FuzzySearchTool(LuceneCpeSearcher luceneSearcher) {
+		this.luceneSearcher=luceneSearcher;
+	}
 	
 	public List<CpeName> searchForLikelyCpeName(String vendorSearchString, String productSearchString, String versionSearchString) {
 		List<CpeEntry> filteredCpes = new ArrayList<>();
 		List<CpeName> likelyCpeNames = new ArrayList<>();
 		try {
 			filteredCpes = luceneSearcher.multiFieldSearch(vendorSearchString, productSearchString, versionSearchString);
-			logger.info("Found likely cpes: "+filteredCpes.size());
+			log.info("Found likely cpes: {}",filteredCpes.size());
 		}catch (Exception e) {
-			logger.debug(e.getMessage());
-			logger.debug("Exception occured while searching for likely cpes !!");
+			log.debug(e.getMessage());
+			log.debug("Exception occured while searching for likely cpes !!");
 		}
-		if (filteredCpes.size() > 0) {
+		if (!filteredCpes.isEmpty()) {
 			for (CpeEntry entry : filteredCpes) {
 				CpeName wrkCpeNameModel = new CpeName();
 				wrkCpeNameModel.setProduct(entry.getProduct());
@@ -42,7 +42,7 @@ public class FuzzySearchTool {
 				}
 				likelyCpeNames.add(wrkCpeNameModel);
 			}
-			logger.debug("Total likely CPEs found: " + filteredCpes.size());
+			log.debug("Total likely CPEs found: " + filteredCpes.size());
 		}
 		return likelyCpeNames;
 	}
