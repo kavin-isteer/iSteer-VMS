66a0f00c4c0a9d8ff8db27b32bd3999650eacaad
kavin-isteer <kavinkr.isteer.com>
2025-07-31 15:36:44 +0530

    Added vulnerabilityEmailPreparator in email service.

--- iSteer-VMS/src/main/java/com/isteer/vms/service/EmailService.java
+++ iSteer-VMS/src/main/java/com/isteer/vms/service/EmailService.java
@@ -1,7 +1,12 @@
 package com.isteer.vms.service;
 
+import java.util.List;
+
+import com.isteer.vms.dto.ComputerResponseDto;
+
 public interface EmailService {
 
-	void sendEmail(String toEmail, String subject, String emailBody);
+	void sendVulnEmailNotification(String toEmail,ComputerResponseDto data);
+	void sendVulnEmailNotifications(List<String> toEmail,ComputerResponseDto data);
 
 }
--- iSteer-VMS/src/main/java/com/isteer/vms/service/impl/EmailServiceImpl.java
+++ iSteer-VMS/src/main/java/com/isteer/vms/service/impl/EmailServiceImpl.java
@@ -1,14 +1,18 @@
 package com.isteer.vms.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
+import com.isteer.vms.dto.ComputerResponseDto;
 import com.isteer.vms.service.EmailService;
 
-import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMessage.RecipientType;
 @Service
 public class EmailServiceImpl implements EmailService{
 	private JavaMailSender javaMailSender;
@@ -21,19 +25,46 @@ public class EmailServiceImpl implements EmailService{
 	}
 	
 	@Override
-	public void sendEmail(String to,String subject,String emailBody) {
-		MimeMessage message = javaMailSender.createMimeMessage();
-		MimeMessageHelper helper;
+	public void sendVulnEmailNotification(String to, ComputerResponseDto data) {
 		try {
-			helper = new MimeMessageHelper(message, true);
-			helper.setFrom(fromEmail);
-			helper.setTo(to);
-			helper.setSubject(subject);
-			helper.setText(emailBody, true); // true indicates HTML content
-			javaMailSender.send(message);
-		} catch (MessagingException e) {
+			javaMailSender.send(new vulnerabilityEmailPreparator(to,data));
+		} catch (Exception e) { 
 			e.printStackTrace();
 		}
 		
+	}
+
+	@Override
+	public void sendVulnEmailNotifications(List<String> toEmail, ComputerResponseDto data) {
+		for(String email : toEmail) {
+			try {
+				javaMailSender.send(new vulnerabilityEmailPreparator(email, data));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+}
+
+class vulnerabilityEmailPreparator implements MimeMessagePreparator{
+	private String toEmail;
+	private String subject;
+	private String emailBody;
+	
+	public vulnerabilityEmailPreparator(String toEmail, ComputerResponseDto data) {
+		this.toEmail = toEmail;
+		
+		//Set subject
+		
+		//Set email body
+	}
+	
+	@Override
+	public void prepare(MimeMessage mimeMessage) throws Exception {
+		mimeMessage.setFrom(emailBody);
+		mimeMessage.setRecipient(RecipientType.TO, new InternetAddress(toEmail));
+		mimeMessage.setSubject(subject);
+		mimeMessage.setText(emailBody, "text/html");
 	}
 }
